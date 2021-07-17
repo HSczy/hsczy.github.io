7f54017d810472af71ebaedd160e072fd81c56a3
HSczy <hsczyic@gmail.com>
2021-07-17 22:02:40 +0800

    更改部分内容

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
-  title: "",
-  description: '9点起床，9点睡觉，每天工作6个小时',
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: "zh-CN", // 将会被设置为 <html> 的 lang 属性
+      title: "学而思而行",
+      description: '9点起床，9点睡觉，每天工作6个小时',
+    }
+  }, 
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }],
@@ -8,10 +15,13 @@ module.exports = {
   ],
   theme: 'reco',
   themeConfig: {
+    mode: 'dark', // 默认 auto，auto 跟随系统，dark 暗色模式，light 亮色模式
+    modePicker: false,
     nav: [
       { text: '主页', link: '/', icon: 'reco-home' },
       { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
-      { text: '联系方式', 
+      {
+        text: '联系方式',
         icon: 'reco-message',
         items: [
           { text: 'GitHub', link: 'https://github.com/HSczy', icon: 'reco-github' }
@@ -32,13 +42,32 @@ module.exports = {
     },
     friendLink: [
     ],
-    
+    locales: {
+      '/': {
+        lang: "zh-CN",
+        recoLocales: {
+          homeBlog: {
+            article: '文章', // 默认 文章
+            tag: '标识', // 默认 标签
+            category: '类别', // 默认 分类
+            friendLink: '友链' // 默认 友情链接
+          },
+          pagation: {
+            prev: '上一页',
+            next: '下一页',
+            go: '前往',
+            jump: '跳转至'
+          }
+        }
+      }
+    },
     logo: '/logo.png',
     // 搜索设置
     search: true,
     searchMaxSuggestions: 10,
     // 自动形成侧边导航
     sidebar: 'auto',
+    sidebarDepth:2,
     // 最后更新时间
     lastUpdated: '最近更新于',
     // 作者
@@ -69,5 +98,5 @@ module.exports = {
   },
   markdown: {
     lineNumbers: true
-  }
-}  
+  },
+}
